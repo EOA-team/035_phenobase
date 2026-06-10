47893fe698a129dbf8b078b71a0259444748f78c
paACode <ackermann.pascal@gmail.com>
2026-06-10 12:25:58 +0200

    chore: fixing autopep8

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-def greet(name:str) -> str:
+"""
+Docstring for src.main
+"""
+def greet(name: str) -> str:
+    """Greet the user by name."""
     return f"Hello, {name}!"
+
 
 if __name__ == "__main__":
     print(greet("Phenobase"))
--- test/test_ci.py
+++ test/test_ci.py
@@ -1,3 +1,6 @@
+"""
+Docstring for test.test_ci
+"""
 def test_ci_works():
+    """Test that the CI pipeline is working correctly."""
     assert True
-
