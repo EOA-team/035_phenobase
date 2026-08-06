dc7640cca9102f0d7ddc81bd813fb425b1b992ad
paACode <ackermann.pascal@gmail.com>
2026-08-06 16:04:05 +0200

    bug: problem fixed

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+This is a workaround to prevent rasterio import issues
+on the Drone Station.
+
+More Info: https://github.com/EOA-team/035_phenobase/issues/14
+"""
+
+import os
+
+print(os.environ["PATH"])
+os.environ["PATH"] = os.pathsep.join(
+    p for p in os.environ["PATH"].split(os.pathsep) if "MATLAB Runtime" not in p
+)
