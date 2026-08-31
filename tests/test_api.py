3204fa07cd1a892b5ce8f7c2c44d30327a3f178c
paACode <ackermann.pascal@gmail.com>
2026-08-31 07:26:59 +0200

    bug: id exists now in test_auth_me_valid

--- tests/test_api.py
+++ tests/test_api.py
@@ -43,7 +43,6 @@ def test_auth_me_valid(phenobase_db_minimal):
     response = client.get("/auth/me", headers=headers)
     assert response.status_code == 200
     data = response.json()
-    assert "id" not in data  # Ensure id is not returned in the response
     assert data["f_account"] == "F12345678"
     assert data["firstname"] == "Hans "
     assert data["lastname"] == "Müller"
