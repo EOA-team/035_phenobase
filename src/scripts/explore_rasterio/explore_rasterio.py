0710be1af185715dbe40a593ade6a55e6facc41f
paACode <ackermann.pascal@gmail.com>
2026-09-22 07:10:44 +0200

    chore: ruff format

--- src/scripts/explore_rasterio/explore_rasterio.py
+++ src/scripts/explore_rasterio/explore_rasterio.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 
 import numpy as np
+import panel as pn
 import rioxarray
 import xarray as xr
 from matplotlib import pyplot as plt
-import hvplot.xarray 
-import panel as pn
 
 
 def fetch_wavelengths(da: xr.DataArray) -> np.ndarray:
@@ -49,25 +48,26 @@ if __name__ == "__main__":
     da = load_orthomosaic(filepath=filename)
     plot_spectrum_at_pixel(
         da, ix=100, iy=200, filepath=Path(__file__).parent / "spectrum_plot.png"
-    )  
+    )
 
     b550 = da.sel(wavelength=550, method="nearest")
     b550.plot.imshow(cmap="gray", robust=True)
     plt.savefig(Path(__file__).parent / "band_550_plot.png")
 
-
     reflectance = da
-    browser= reflectance.hvplot.image(
-         x="x", y="y", groupby="band",     # <- slider over all 487 bands
-         cmap="gray", robust=True,
-         widget_type="scrubber", widget_location="bottom",
-         framewise =False,
-         width=1400, height=700,
-     )
+    browser = reflectance.hvplot.image(
+        x="x",
+        y="y",
+        groupby="band",  # <- slider over all 487 bands
+        cmap="gray",
+        robust=True,
+        widget_type="scrubber",
+        widget_location="bottom",
+        framewise=False,
+        width=1400,
+        height=700,
+    )
     pn.serve(browser, title="Orthomosaic Reflectance Browser", port=5006, show=True)
-
-
-
 
     # spec.sel(wavelength=slice(400,1000)).plot.line(x="wavelength", y="band", hue="band", marker="o")
     # b1 = da.sel(band=1)
